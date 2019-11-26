cb8601c1e10fe2425d62f3214c7cbbd2ba61ed2e
Ante Mihalj <ante.mihalj@gmail.com>
2019-11-26 17:52:38 +0100

    Add support for `token_endpoint_auth_method` (#37)

--- hydra/types.go
+++ hydra/types.go
@@ -4,13 +4,14 @@ import "k8s.io/utils/pointer"
 
 // OAuth2ClientJSON represents an OAuth2 client digestible by ORY Hydra
 type OAuth2ClientJSON struct {
-	ClientID      *string  `json:"client_id,omitempty"`
-	Secret        *string  `json:"client_secret,omitempty"`
-	GrantTypes    []string `json:"grant_types"`
-	RedirectURIs  []string `json:"redirect_uris,omitempty"`
-	ResponseTypes []string `json:"response_types,omitempty"`
-	Scope         string   `json:"scope"`
-	Owner         string   `json:"owner"`
+	ClientID                *string  `json:"client_id,omitempty"`
+	Secret                  *string  `json:"client_secret,omitempty"`
+	GrantTypes              []string `json:"grant_types"`
+	RedirectURIs            []string `json:"redirect_uris,omitempty"`
+	ResponseTypes           []string `json:"response_types,omitempty"`
+	Scope                   string   `json:"scope"`
+	Owner                   string   `json:"owner"`
+	TokenEndpointAuthMethod string   `json:"token_endpoint_auth_method,omitempty"`
 }
 
 // Oauth2ClientCredentials represents client ID and password fetched from a Kubernetes secret
