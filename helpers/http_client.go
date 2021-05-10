27558c39fa1bf8bba58618ef7d8abaf8ef2c4d4e
fjviera <javier.viera@mindcurv.com>
2021-05-10 09:27:21 +0200

    Improve error handling

--- helpers/http_client.go
+++ helpers/http_client.go
@@ -3,13 +3,14 @@ package helpers
 import (
 	"crypto/tls"
 	"net/http"
+	"os"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	httptransport "github.com/go-openapi/runtime/client"
 )
 
-func CreateHttpClient(insecureSkipVerify bool, tlsTrustStore string) *http.Client {
+func CreateHttpClient(insecureSkipVerify bool, tlsTrustStore string) (*http.Client, error) {
 	setupLog := ctrl.Log.WithName("setup")
 	tr := &http.Transport{}
 	httpClient := &http.Client{}
@@ -19,6 +20,10 @@ func CreateHttpClient(insecureSkipVerify bool, tlsTrustStore string) *http.Clien
 		httpClient.Transport = tr
 	}
 	if tlsTrustStore != "" {
+		if _, err := os.Stat(tlsTrustStore); err != nil {
+			return nil, err
+		}
+
 		setupLog.Info("configuring TLS with tlsTrustStore")
 		ops := httptransport.TLSClientOptions{
 			CA:                 tlsTrustStore,
@@ -26,8 +31,8 @@ func CreateHttpClient(insecureSkipVerify bool, tlsTrustStore string) *http.Clien
 		}
 		if tlsClient, err := httptransport.TLSClient(ops); err != nil {
 			setupLog.Error(err, "Error while getting TLSClient, default http client will be used")
-			return tlsClient
+			return tlsClient, nil
 		}
 	}
-	return httpClient
+	return httpClient, nil
 }
